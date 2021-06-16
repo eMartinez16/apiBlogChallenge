e7dabcdda4dfe40fae6b79beabb4367f712b1cb3
Emiliano Martinez <emartinezd16@gmail.com>
2021-06-16 07:44:55 -0300

    update of the postController file, adding the methods to perform the requests

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,12 +1,67 @@
-const postModel = require('../../database');
+const { post } = require('../../database');
 
 const getPost = async (req, res) => {
   const query = req.query;
   console.log(query);
-
   try {
-    if(Object.keys(query).length)
+    posts = await post.findAll({
+      attributes: ['id', 'title', 'image', 'categoryId', 'createdAt'],
+      order: [['title', 'desc']],
+    });
+    res.json(posts);
   } catch (error) {
-    
+    console.error(error);
   }
 };
+
+const getPostById = async (req, res) => {
+  try {
+    const posts = await post.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!posts) {
+      res.json({ message: 'post does not exists' });
+    }
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createPost = async (req, res) => {
+  try {
+    const newPost = await post.create(req.body);
+    res.json(newPost);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const deletePost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await post.destroy({
+      where: {
+        id,
+      },
+    });
+    res.json({ msg: 'post deleted' });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    await post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ msg: 'post updated' });
+  } catch (error) {
+    console.error(error);
+  }
+};
+module.exports = { getPost, getPostById, deletePost, updatePost, createPost };
