1d1b7397bae1e613d6593e4b301fa98a5ba33087
Emiliano Martinez <emartinezd16@gmail.com>
2021-06-16 06:55:01 -0300

    database file with the connection and the relation of the tables

---
+++ database.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv/config');
+const postModel = require('./src/models/postModel');
+const categoryModel = require('./src/models/categoryModel');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+const post = postModel(sequelize, DataTypes);
+const category = categoryModel(sequelize, DataTypes);
+
+post.belongsTo(category);
+category.hasMany(post);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection success.');
+    sequelize.sync();
+  })
+  .catch(error => console.error('Unable to connect to the database:', error));
+
+module.exports = { post, category };
