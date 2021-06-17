e6a53058c19acfaf693da885c4e6f0ad337ff18d
Emiliano Martinez <emartinezd16@gmail.com>
2021-06-17 02:35:12 -0300

    I changed the name of the ID field in the post model as it caused problems when an id query was made (since with the above property, that id was encrypted).

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,25 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('Post', {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true,
     },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     content: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     image: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
     categoryId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
     },
   });
 };
